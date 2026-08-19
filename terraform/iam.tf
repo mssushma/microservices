39d96f89ca74122f9613958e66673062b188c1d6
mssushma <87899596+mssushma@users.noreply.github.com>
2026-08-19 17:39:56 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -16,3 +16,10 @@ resource "aws_iam_role" "ec2_role" {
     }]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+
+  role       = aws_iam_role.ec2_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
