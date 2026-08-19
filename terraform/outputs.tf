77f652a049712da6c78f5ac79ec6416ece3ce940
mssushma <87899596+mssushma@users.noreply.github.com>
2026-08-19 18:06:25 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,19 @@ output "instance_id" {
 
   value = aws_instance.microservices.id
 }
+
+output "user_ecr_uri" {
+  value = aws_ecr_repository.user_repo.repository_url
+}
+
+output "product_ecr_uri" {
+  value = aws_ecr_repository.product_repo.repository_url
+}
+
+output "order_ecr_uri" {
+  value = aws_ecr_repository.order_repo.repository_url
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.k8s_server.public_ip
+}
